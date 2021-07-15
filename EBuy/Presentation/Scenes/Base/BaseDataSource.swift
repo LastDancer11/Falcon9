ea45c90aed8e31e5e3dcde86a59363beb687d187
LastDancer11 <meskhii11t@gmail.com>
2021-07-15 12:58:50 +0300

    -rework phase 1.1 everythings fine

--- EBuy/Presentation/Scenes/Base/BaseDataSource.swift
+++ EBuy/Presentation/Scenes/Base/BaseDataSource.swift
@@ -9,12 +9,21 @@ import UIKit
 
 class BaseDataSource: NSObject {
     
-    internal var singleSectionModels: [CellItem]!
-    internal var multiSectionModels: [[CellItem]]!
+    var singleSectionModels: [CellItem]!
+    var multiSectionModels: [[CellItem]]!
     
-    internal weak var tableView: UITableView? {
+    var singleCollectionSectionModels: [CellItem]!
+    var multiCollectionSectionModels: [[CellItem]]!
+    
+    weak var tableView: UITableView? {
         didSet {
             tableView?.dataSource = self
+        }
+    }
+    
+    weak var collectionView: UICollectionView? {
+        didSet {
+            collectionView?.dataSource = self
         }
     }
     
@@ -22,11 +31,16 @@ class BaseDataSource: NSObject {
         
     }
     
-    func initCustomCell(for indexPath: IndexPath, with cellIdentifier: String) -> UITableViewCell? {
+    func initCustomTableViewCell(for indexPath: IndexPath, with cellIdentifier: String) -> UITableViewCell? {
+        return nil
+    }
+    
+    func initCustomCollectionViewCell(for indexPath: IndexPath, with cellIdentifier: String) -> UICollectionViewCell? {
         return nil
     }
 }
 
+// MARK: UITable View Data Source
 extension BaseDataSource: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -40,12 +54,12 @@ extension BaseDataSource: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items(for: section).count
+        return itemsForTableView(for: section).count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cellViewModel = items(for: indexPath.section)[indexPath.row]
+        let cellViewModel = itemsForTableView(for: indexPath.section)[indexPath.row]
         let cell: UITableViewCell
         
         
@@ -63,21 +77,21 @@ extension BaseDataSource: UITableViewDataSource {
         case HabitsCell.identifier:
             cell = tableView.deque(HabitsCell.self, for: indexPath)
         default:
-            if let _cell = initCustomCell(for: indexPath, with: cellViewModel.cellIdentifier) {
+            if let _cell = initCustomTableViewCell(for: indexPath, with: cellViewModel.cellIdentifier) {
                 cell = _cell
             } else {
                 fatalError()
             }
         }
         
-        if let configurableCell = cell as? CellConfigurable {
+        if let configurableCell = cell as? TableCellConfigurable {
             configurableCell.configure(with: cellViewModel)
         }
         
         return cell
     }
     
-    func items(for section: Int) -> [CellItem] {
+    func itemsForTableView(for section: Int) -> [CellItem] {
         if singleSectionModels != nil {
             return singleSectionModels
         } else if multiSectionModels != nil {
@@ -88,7 +102,71 @@ extension BaseDataSource: UITableViewDataSource {
     }
 }
 
-protocol CellConfigurable: UITableViewCell {
+// MARK: UICollectionView Data Source
+
+extension BaseDataSource: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        if singleCollectionSectionModels != nil {
+            return 1
+        } else if multiCollectionSectionModels != nil {
+            return multiCollectionSectionModels.count
+        } else {
+            fatalError("No models set up!")
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsForCollection(for: section).count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cellViewModel = itemsForCollection(for: indexPath.section)[indexPath.row]
+        let cell: UICollectionViewCell
+        
+        switch cellViewModel.cellIdentifier {
+        case CategoryItemCell.identifier:
+            cell = collectionView.deque(CategoryItemCell.self, for: indexPath)
+        case RecentlyViewedItemCell.identifier:
+            cell = collectionView.deque(RecentlyViewedItemCell.self, for: indexPath)
+        case SavedItemCell.identifier:
+            cell = collectionView.deque(SavedItemCell.self, for: indexPath)
+        case BrandsItemCell.identifier:
+            cell = collectionView.deque(BrandsItemCell.self, for: indexPath)
+        case HabitItemCell.identifier:
+            cell = collectionView.deque(HabitItemCell.self, for: indexPath)
+        default:
+            if let _cell = initCustomCollectionViewCell(for: indexPath, with: cellViewModel.cellIdentifier) {
+                cell = _cell
+            } else {
+                fatalError()
+            }
+        }
+        
+        if let configurableCell = cell as? CollectionCellConfigurable {
+            configurableCell.configure(with: cellViewModel)
+        }
+        
+        return cell
+    }
+    
+    func itemsForCollection(for section: Int) -> [CellItem] {
+        if singleCollectionSectionModels != nil {
+            return singleCollectionSectionModels
+        } else if multiCollectionSectionModels != nil {
+            return multiCollectionSectionModels[section]
+        } else {
+            fatalError("No models set up!")
+        }
+    }
+}
+
+// MARK: Setupers
+protocol TableCellConfigurable: UITableViewCell {
+    func configure(with item: CellItem)
+}
+
+protocol CollectionCellConfigurable: UICollectionViewCell {
     func configure(with item: CellItem)
 }
 
@@ -112,4 +190,3 @@ struct CellViewModel: CellItem {
 enum CellViewModelUserDataKey {
     case data
 }
-
